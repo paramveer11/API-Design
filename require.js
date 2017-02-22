221811f8d86445aec586a2642a2cd25534c7abbc
Paramveer Singh <paramveersingh@Paramveers-MacBook-Air.local>
2017-02-22 15:14:41 +0530

    Task Complete

--- require.js
+++ require.js
@@ -1,8 +1,12 @@
-var CLIENT_ID = '742818722171-lavakgg8sc54q277tvdg4siao891qe26.apps.googleusercontent.com';//Respective ClientID should be mentioned
+//Respective ClientID should be mentioned,Replace it with a new clientID obtained from console.developers.google.com
+var CLIENT_ID = '806264380803-b3p93b9804hf2lt53n96lbp21a7got2h.apps.googleusercontent.com';
   var SCOPES_GA = ["https://www.googleapis.com/auth/analytics.readonly"];
   var SCOPES_GTM = ["https://www.googleapis.com/auth/tagmanager.readonly"];
   var JSONGA = [];
   var JSONGTM = [];
+  var count = 0;
+  var i = 0;
+  var j = 0;
 
 //Function to Authorize user for accessing Google Analytics 
   function authorizeGa(event) {
@@ -24,6 +28,7 @@ var CLIENT_ID = '742818722171-lavakgg8sc54q277tvdg4siao891qe26.apps.googleuserco
       }
     });
   }
+
 //Function to Authorize user for accessing Google Tag Manager
   function authorizeGtm(event) {
     var useImmdiate = event ? false : true;
@@ -58,43 +63,43 @@ function queryAccountsGtm(){
     gapi.client.tagmanager.accounts.list().then(listAccountsGtm);
   });
 }
-//Listing All Acount Details of the user in GA
+
+//Listing All Acount Details of the user in GA..
 function listAccountsGa() {
   var request = gapi.client.analytics.management.accounts.list();
-  request.execute(printAccountsGa);
-  console.log("Array",JSONGA);
+  request.execute(printAccountsGa);  
 }
-//Listing All Acount Details of the user in GTM
+
+//Listing All Acount Details of the user in GTM..
 function listAccountsGtm() {
   var request = gapi.client.tagmanager.accounts.list();
   request.execute(printAccountsGtm);
 }
 
+//Printing Accounts of GA as per the AccountID which is a parameter accepting results as arguments...
 function printAccountsGa(results) {
   if (results && !results.error) {
     var accounts = results.items;
-    for (var i = 0, account; account = accounts[i]; i++) {
-      //console.log('Account Id: ' + account.id);
+    var account;
+    for ( i = 0,account; account = accounts[i]; i++) {
       listProperties(account.id);
-      //console.log('Account Name: ' + account.name);
     }
-  }
-  csvConverterGA(JSONGA);
+  }  
 }
 
+//Printing Accounts of GTM as per the AccountID which is a parameter accepting results as arguments...
 function printAccountsGtm(results) {
-    console.log("result is",results);
     if (results && !results.error) {
     var accounts = results.accounts;
-    console.log(accounts);
-    for (var i = 0, account; account = accounts[i]; i++) {
-      //console.log('Account Id: ' + account.accountId);
+    var account;
+    count = 0;
+    for ( j = 0, account; account = accounts[j]; j++) {
       listContainers(account.accountId);
-      //console.log('Account Name: ' + account.name);
     }
   }
 }
 
+//Listing properties of GA linked to the particular accountID which is passed as argument
 function listProperties(accountID){
   var request = gapi.client.analytics.management.webproperties.list({
     'accountId': accountID
@@ -102,60 +107,83 @@ function listProperties(accountID){
   request.execute(printProperties);
 }
 
+//Printing properties of GA iterating over different properties of the accountID
 function printProperties(results){
+  count++;
   if (results && !results.error) {
     var properties = results.items;
-    var tempObj={};
-    //console.log(properties[0].accountId);
+    if(properties.length > 0){
+      var tempObj={};
     $.each(properties,function(element){
-      //console.log(properties[element].accountId);
       tempObj={
         'AccountID':properties[element].accountId,
         'PropertyID':properties[element].id,
         'Name':properties[element].name
       };
-      console.log(element);
-      JSONGA.push(tempObj);
+      JSONGA.push(tempObj);//Push the required properties to a global array
     })
+  }    
+ }
+ if (count == i){
+  csvConverterGA(JSONGA);//Pass this to a function which converts it to a CSV
  } 
 }
 
-
+//Listing containers of GTM linked to the particular accountID which is passed as argument
 function listContainers(accountID){
-  console.log("accountID for containers",accountID);
   var request = gapi.client.tagmanager.accounts.containers.list({
     'accountId' : accountID
   });
   request.execute(printContainers);
 }
 
+//Printing container properties of GTM iterating over different container properties of the accountID
 function printContainers(results){
+  count++;
   if(results && !results.error){
     var containers = results.containers;
-    for(var i = 0 , container; container = containers[i] ; i++){
-      console.log('ContainerId:', container.containerId);
-      console.log("PublicId: ", container.publicId);
-    }
+    var tempObj = {};
+    $.each(containers,function(element){
+      tempObj={
+        'AccountID':containers[element].accountId,
+        'ContainerID':containers[element].containerId,
+        'PublicID':containers[element].publicId
+      };
+      JSONGTM.push(tempObj);
+    })    
+  }
+  if(count == j){
+    csvConverterGTM(JSONGTM);
   }
 }
 
-
+//Function to get GA properties in a CSV
 function csvConverterGA(array){
   var jsonObjects = JSON.stringify(array);
-  console.log("JSON",jsonObjects);
-   var result = '';
-   array = JSON.parse(jsonObjects);
-   //console.log(array);
-      array.forEach(function(k,i){
+  var result = '';
+  array = JSON.parse(jsonObjects);
+  var headers = ["AccountID","PropertyID","Name"].join(",");
+  result = headers + "\r\n";
+  array.forEach(function(k,i){
       result += k.AccountID + "," + k.PropertyID + "," + k.Name + "\r\n";
-      console.log(result)
-    });
-   
-    console.log("convert",result);
+    });   
 
-    //window.open('data:text/csv;charset=utf-8,' + escape(result));
+    window.open('data:text/csv;charset=utf-8,' + escape(result));
 }
 
+//Function to get GTM properties in a CSV
+function csvConverterGTM(array){
+  var jsonObjects = JSON.stringify(array);
+  var result = '';
+  array = JSON.parse(jsonObjects);
+  var headers = ["AccountID","ContainerID","PublicID"].join(",");
+  result = headers + "\r\n";
+  array.forEach(function(k,i){
+    result += (k.AccountID + "," + k.ContainerID + "," + k.PublicID + "\r\n");
+  });
+
+  window.open("data:text/csv;charset=uth-8," + escape(result));
+}
 
 window.onload = function(){
   var el = document.getElementById('auth-button-ga');
